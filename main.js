2713058671cd776217bb37da97af234f8f6372f0
comseung18 <comseung18@koreatech.ac.kr>
2022-03-26 21:48:38 +0900

    간트차트빼고 프론트완성

--- main.js
+++ main.js
@@ -10,26 +10,10 @@ function compare_process_arrival_time(p1, p2)
 var app = new Vue({
     el : "#app",
     data:{
-        algorithm: 'FCFS',
-        quantum_time: 0,
-        arrival_time: 0,
-        burst_time:0,
-        process_list: []
     },
     computed:{
-        sorted_process_list: function(){
-            this.process_list.sort(compare_process_arrival_time);
-            return this.process_list;
-        }
     }
     ,
     methods:{
-        process_add()
-        {
-            this.process_list.push({
-                "arrival_time" : this.arrival_time,
-                "burst_time": this.burst_time,
-            });
-        }
     }
 });
