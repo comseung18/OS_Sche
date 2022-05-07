8093ea1682cd0d8795c7faeb98e83d0940abd4dc
comseung18 <comseung18@koreatech.ac.kr>
2022-05-07 23:24:40 +0900

    sample5 적절

--- js/sample/sample5.js
+++ js/sample/sample5.js
@@ -3,9 +3,9 @@ function sample5()
     sample_btn_active("sample5");
     //hrrn
     app.only_table_reset();
-    app.process_add_by_param(0,4);
-    app.process_add_by_param(2,5);
-    app.process_add_by_param(4,3);
-    app.process_add_by_param(6,6);
-    app.process_add_by_param(8,3);
+    app.process_add_by_param(0,2);
+    app.process_add_by_param(1,2);
+    app.process_add_by_param(1,10);
+    app.process_add_by_param(4,4);
+    app.process_add_by_param(8,4);
 }
